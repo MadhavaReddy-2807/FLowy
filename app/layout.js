975e0a5eacd8d171d470aad75b96f168981a3ba5
Madhava Reddy <madhava2807@gmail.com>
2025-06-23 22:50:18 +0530

    f

--- app/layout.js
+++ app/layout.js
@@ -50,7 +50,6 @@ export default function RootLayout({ children }) {
             <Toaster />
             {children}
           </ClerkLoaded>
-         
         </body>
       </html>
     </ClerkProvider>
